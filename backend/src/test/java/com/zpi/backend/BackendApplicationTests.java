dc4d7a5031e3d34bec851289d9153a9e73a9b39d
Paulina Drzazga <paulina@halpress.team>
2023-09-13 07:36:01 +0200

    Initialised spring project

---
+++ backend/src/test/java/com/zpi/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zpi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
