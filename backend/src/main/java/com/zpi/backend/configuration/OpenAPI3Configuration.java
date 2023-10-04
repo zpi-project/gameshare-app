ef74b79a0c815a21b0f0dd44e14d94e371293ffc
Paulina Drzazga <91901843+pdrzazgaa@users.noreply.github.com>
2023-10-04 14:02:24 +0200

    Zpi 58 swagger redoc implementation (#9)
    * Added .gitignore for entire project
    
    * Configurated Swagger and Redoc
    
    ---------
    
    Co-authored-by: Paulina Drzazga <paulina@halpress.team>

---
+++ backend/src/main/java/com/zpi/backend/configuration/OpenAPI3Configuration.java
@@ -0,0 +1,31 @@
+package com.zpi.backend.configuration;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "localhost")},
+        info = @Info(title = "GameShare App", version = "1.0"))
+public class OpenAPI3Configuration {
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "Token Authorization";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
