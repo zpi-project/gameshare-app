11c900f2d4a3adaf1d9e01dcfdd085c183fe8a64
Aleksandra <aleserwicka@gmail.com>
2023-10-06 18:31:33 +0200

    test

--- terraform/cloud-sql.tf
+++ terraform/cloud-sql.tf
@@ -4,6 +4,12 @@ resource "google_sql_database" "database" {
   name            = "test-db"
 }
 
+resource "google_sql_database" "database-test" {
+  project         = var.project
+  instance        = google_sql_database_instance.primary.name
+  name            = "test-db-test"
+}
+
 resource "google_sql_database_instance" "primary" {
   project             = var.project
   name                = "cloud-sql-instance"
