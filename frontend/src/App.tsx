76a3e85d69ee054d66860aeb43b601845e2e7fd5
Maria Markowiak <maria.markowiak@gmail.com>
2023-10-06 18:26:49 +0200

    add clsx library, fix prettier config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { VITE_CLIENT_ID } from "./constants/env";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@components/ThemeProvider";
+import { VITE_CLIENT_ID } from "./constants/env";
 
 const queryClient = new QueryClient();
 
@@ -11,11 +11,11 @@ const App: FC = () => {
   return (
     <GoogleOAuthProvider clientId={VITE_CLIENT_ID}>
       <QueryClientProvider client={queryClient}>
-          <ThemeProvider defaultTheme="dark">
+        <ThemeProvider defaultTheme="dark">
           <div className="h-full w-full">
             <Outlet />
           </div>
-          </ThemeProvider>
+        </ThemeProvider>
       </QueryClientProvider>
     </GoogleOAuthProvider>
   );
