6344193dfa91b7beca8d4c8d16b124ed6a217e43
Maria Markowiak <maria.markowiak@gmail.com>
2023-10-10 15:53:06 +0200

    add reading envs from docker env file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,12 @@ import { Outlet } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@components/ThemeProvider";
-import { VITE_CLIENT_ID } from "./constants/env";
 
 const queryClient = new QueryClient();
 
 const App: FC = () => {
   return (
-    <GoogleOAuthProvider clientId={VITE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_AUTH_CLIENT_ID}>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider defaultTheme="dark">
           <div className="h-full w-full">
