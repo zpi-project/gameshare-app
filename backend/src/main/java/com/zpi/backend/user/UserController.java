2503a43a45624d84e297d17bf46d4373411003ed
Paulina Drzazga <paulina@halpress.team>
2023-10-11 18:09:59 +0200

    Added CORS to UserController

--- backend/src/main/java/com/zpi/backend/user/UserController.java
+++ backend/src/main/java/com/zpi/backend/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.security.core.Authentication;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin("${FRONTEND_HOST}:${FRONTEND_PORT}")
 public class UserController {
 
     @Autowired
