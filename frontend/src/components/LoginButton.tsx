cdcdc068335dade85c354c25d6e8911a69d9f49e
Maria Markowiak <91902077+MarkowiakM@users.noreply.github.com>
2023-10-11 19:18:59 +0200

    [ZPI-81] typography & theme + small bugfix in setting roles (#22)
    * define theme colors
    
    * import google font
    
    * fix logging, cause i broke it
    
    * remove test api request

--- frontend/src/components/LoginButton.tsx
+++ frontend/src/components/LoginButton.tsx
@@ -2,13 +2,19 @@ import { FC } from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { useSetRecoilState } from "recoil";
 import { tokenState } from "@state/token";
+import { roleState } from "@state/role";
 
 const LoginButton: FC = () => {
   const setToken = useSetRecoilState(tokenState);
+  const setRole = useSetRecoilState(roleState);
 
   return (
     <GoogleLogin
       onSuccess={res => {
+        if (res.credential) {
+          setRole("user");
+          setToken(res.credential);
+        }
         setToken(res.credential ?? null);
       }}
       onError={() => {
