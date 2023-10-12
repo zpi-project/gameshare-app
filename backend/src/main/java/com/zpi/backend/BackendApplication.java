f46ebaee8e47982e62d4eb11690bca0eb940c584
JakubSamulski <kubasamulski@gmail.com>
2023-10-12 22:20:14 +0200

    deleted cors in main

--- backend/src/main/java/com/zpi/backend/BackendApplication.java
+++ backend/src/main/java/com/zpi/backend/BackendApplication.java
@@ -12,14 +12,5 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/test").allowedOrigins("http://localhost:5173");
-			}
-		};
-	}
 
 }
