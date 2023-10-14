7b5e8f6613e321cda61422c24128d477568c4bcc
Maria Markowiak <91902077+MarkowiakM@users.noreply.github.com>
2023-10-14 15:56:56 +0200

    [ZPI-69] frontend main layout + sidenav (#25)
    * add layout
    
    * add urls
    
    * install missing ui libraries, add button component from shadcn
    
    * add sidenav layout
    
    * add SideNavLink component
    
    * add user popover with login / logout buttons

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "cypress.config.ts", "postcss.config.js"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "spaced-comment": "error",
+    "no-duplicate-imports": "error",
+    "prefer-template": "warn",
+    "no-param-reassign": "warn",
+    "no-redeclare": "warn",
+    "no-useless-rename": "warn",
+    "prefer-spread": "warn",
+    "no-implicit-coercion": "warn",
+    "prefer-const": "warn",
+    quotes: ["error", "double", { avoidEscape: true }],
+    "@typescript-eslint/unbound-method": ["error", { ignoreStatic: true }],
+    "@typescript-eslint/no-confusing-void-expression": ["error", { ignoreArrowShorthand: true }],
+    "@typescript-eslint/no-extraneous-class": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "warn",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  overrides: [
+    {
+      files: ["./src/components/ui/**/*.tsx"],
+      extends: ["./src/components/ui/.eslintrc.cjs"],
+    },
+  ],
+};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,23 @@
-import { FC, useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { FC } from "react";
+import Layout from "@/components/Layout";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { roleState } from "@state/role";
-import { tokenState } from "@state/token";
-import Api from "@api/Api";
-import { ThemeProvider } from "@components/ThemeProvider";
+import { RecoilRoot } from "recoil";
 
 const queryClient = new QueryClient();
 
 const App: FC = () => {
-  const setRole = useSetRecoilState(roleState);
-  const token = useRecoilValue(tokenState);
-
-  useEffect(() => {
-    const tokenInterceptor = Api.interceptors.request.use(config => {
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    });
-
-    return () => {
-      Api.interceptors.request.eject(tokenInterceptor);
-    };
-  }, [setRole, token]);
-
   return (
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_AUTH_CLIENT_ID}>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider defaultTheme="light">
-          <div className="h-full w-full">
-            <Outlet />
-          </div>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </GoogleOAuthProvider>
+    <RecoilRoot>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_AUTH_CLIENT_ID}>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider defaultTheme="dark">
+            <Layout />
+          </ThemeProvider>
+        </QueryClientProvider>
+      </GoogleOAuthProvider>
+    </RecoilRoot>
   );
 };
 
