9cfd0996bf82aa04bb2a5c5408191bc915a4c748
Maria Markowiak <91902077+MarkowiakM@users.noreply.github.com>
2023-10-17 00:23:03 +0200

    [ZPI-72] Configure leaflet map with dark mode support (#29)
    * add leaflet, configure with leaflet, add dark mode map
    
    * remove unused code

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
-import Spinner from "@/components/ui/Spinner";
+import Map from "@/components/Map/Map";
 
 const Dashboard: FC = () => {
   return (
     <div className="flex h-full w-full flex-row gap-6">
-      <div className="flex-grow rounded-lg bg-section p-4">space for map</div>
+      <div className="flex-grow overflow-hidden rounded-lg bg-section">
+        <Map />
+      </div>
       <div className="w-[550px] rounded-lg bg-section p-4">space for search games</div>
-      <Spinner />
     </div>
   );
 };
