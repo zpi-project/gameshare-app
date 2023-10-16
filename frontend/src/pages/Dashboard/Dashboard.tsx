761cff0eae1f72fb02bd80cf73eccd785d60265c
Maria Markowiak <maria.markowiak@gmail.com>
2023-10-16 15:02:43 +0200

    add leaflet, configure with leaflet, add dark mode map

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
+import Map from "@/components/Map/Map";
 
 const Dashboard: FC = () => {
   return (
     <div className="flex h-full w-full flex-row gap-6">
-      <div className="flex-grow rounded-lg bg-section p-4">space for map</div>
+      <div className="flex-grow overflow-hidden rounded-lg bg-section">
+        <Map />
+      </div>
       <div className="w-[550px] rounded-lg bg-section p-4">space for search games</div>
     </div>
   );
