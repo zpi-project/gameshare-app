c8e0a4ea1b6090a3d708f42100719a0b83c9bb43
Maria Markowiak <maria.markowiak@gmail.com>
2023-10-16 12:53:26 +0200

    add geolocation

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,19 @@
 import { FC } from "react";
+import { useGeolocated } from "react-geolocated";
 
 const Dashboard: FC = () => {
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } = useGeolocated({
+    positionOptions: {
+      enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
+  });
+
   return (
     <div className="flex h-full w-full flex-row gap-6">
-      <div className="flex-grow rounded-lg bg-section p-4">space for map</div>
+      <div className="flex-grow rounded-lg bg-section p-4">
+        space for map: longitude: {coords?.longitude}, latitude: {coords?.latitude}
+      </div>
       <div className="w-[550px] rounded-lg bg-section p-4">space for search games</div>
     </div>
   );
