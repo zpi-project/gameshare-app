d78a49d8326192103e1a3fc0dc7b59ecf57ef672
Maria Markowiak <91902077+MarkowiakM@users.noreply.github.com>
2023-10-17 10:33:30 +0200

    [ZPI-33] add geolocation hook (#28)
    * add geolocation hook

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,23 @@
 import { FC } from "react";
+import { useGeolocated } from "react-geolocated";
 import Map from "@/components/Map/Map";
 
 const Dashboard: FC = () => {
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } = useGeolocated({
+    positionOptions: {
+      enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
+  });
+
   return (
     <div className="flex h-full w-full flex-row gap-6">
       <div className="flex-grow overflow-hidden rounded-lg bg-section">
         <Map />
       </div>
-      <div className="w-[550px] rounded-lg bg-section p-4">space for search games</div>
+      <div className="w-[550px] rounded-lg bg-section p-4">
+        space for search games, longitude: {coords?.longitude}, latitude: {coords?.latitude}
+      </div>
     </div>
   );
 };
