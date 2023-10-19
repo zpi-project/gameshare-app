16ea7ff1b4528d31b4fbbecdc03a4263e9da7c37
Aleksandra <aleserwicka@gmail.com>
2023-10-19 08:11:28 +0200

    Settings page

--- frontend/src/pages/Settings/Settings.tsx
+++ frontend/src/pages/Settings/Settings.tsx
@@ -8,14 +8,14 @@ const Settings: FC = () => {
   return (
     <div className="flex h-full flex-row gap-6">
       <div className="flex h-full max-w-2xl flex-grow flex-col gap-6 rounded-lg">
-        <div className="h-2/5 rounded-lg bg-section p-4">
+        <div className="h-80 rounded-lg bg-section p-4">
           <UserDetails
             onClick={() => setIsModalOpen(true)}
             user={{ firstname: "Aleksandra", lastname: "Serwicka" }}
           />
           {isModalOpen && <EditPersonalDataModal />}
         </div>
-        <div className="flex h-3/5 rounded-lg bg-section">
+        <div className="flex h-80 rounded-lg bg-section">
           <Opinions />
         </div>
       </div>
