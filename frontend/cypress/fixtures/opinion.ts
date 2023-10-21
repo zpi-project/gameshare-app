088a92bf07dd6b8df024ebda04eb6bbbe5b12930
Maria Markowiak <maria.markowiak@gmail.com>
2023-10-21 12:09:40 +0200

    get own data from api, fix user type, move mock data to fixtures

---
+++ frontend/cypress/fixtures/opinion.ts
@@ -0,0 +1,42 @@
+import { LOREM } from "@/constants/lorem";
+import { Opinion } from "@/types/Opinion";
+import { user } from "./user";
+
+
+export const opinions: Opinion[] = [
+  {
+    description: LOREM,
+    stars: 1,
+    user: user,
+  },
+  {
+    description: LOREM,
+    stars: 1,
+    user: user,
+  },
+  {
+    description: LOREM,
+    stars: 1,
+    user: user,
+  },
+  {
+    description: LOREM,
+    stars: 1,
+    user: user,
+  },
+  {
+    description: LOREM,
+    stars: 1,
+    user: user,
+  },
+  {
+    description: LOREM,
+    stars: 1,
+    user: user,
+  },
+  {
+    description: LOREM,
+    stars: 1,
+    user: user,
+  },
+];
---
+++ frontend/cypress/fixtures/user.ts
@@ -0,0 +1,13 @@
+import { User } from "@/types/User";
+
+
+export const user: User = {
+  firstName: "Ola",
+  lastName: "Serwicka",
+  phoneNumber: "string",
+  locationLatitude: 51.12342,
+  locationLongitude: 18.32121,
+  email: "aserwicka@gmail.com",
+  avatarLink:
+    "https://cdn2.vectorstock.com/i/1000x1000/32/01/user-sign-icon-person-symbol-human-avatar-vector-12693201.jpg",
+};
