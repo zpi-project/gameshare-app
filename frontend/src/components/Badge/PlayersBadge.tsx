be36f929c3d4d9d545350e9ab2a6be3655b19c9b
Maria Markowiak <maria.markowiak@gmail.com>
2023-11-05 10:40:28 +0100

    run linter

--- frontend/src/components/Badge/PlayersBadge.tsx
+++ frontend/src/components/Badge/PlayersBadge.tsx
@@ -1,16 +1,21 @@
 import { FC } from "react";
-import { Badge } from "@/components/ui/badge";
 import { User2 } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+
 interface PlayersBadgeProps {
   maxPlayers: number;
   minPlayers: number;
 }
 
 const PlayersBadge: FC<PlayersBadgeProps> = ({ minPlayers, maxPlayers }) => {
-    return <Badge variant="secondary" className="flex flex-row gap-1">
-        <User2 size={20} />
-        <span className="tracking-wider">{minPlayers}-{maxPlayers}</span>
-  </Badge>;
+  return (
+    <Badge variant="secondary" className="flex flex-row gap-1">
+      <User2 size={20} />
+      <span className="tracking-wider">
+        {minPlayers}-{maxPlayers}
+      </span>
+    </Badge>
+  );
 };
 
 export default PlayersBadge;
