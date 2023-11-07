719a22cfb93752ead9891a1b50a3d4673dfbea0e
Maria Markowiak <maria.markowiak@gmail.com>
2023-11-07 14:37:24 +0100

    fix loading state

--- frontend/src/pages/Game/Game.tsx
+++ frontend/src/pages/Game/Game.tsx
@@ -1,16 +1,14 @@
-import { FC, useEffect, useRef } from "react";
+import { FC } from "react";
 import { useTranslation } from "react-i18next";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { URLS } from "@/constants/urls";
 import { stringToHexColor } from "@/utils/stringToColor";
 import { GameApi } from "@/api/GameApi";
-import { TimeBadge, PlayersBadge, AgeBadge } from "@/components/Badge";
 import { useTheme } from "@/components/ThemeProvider";
-import { Badge } from "@/components/ui/badge";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Skeleton } from "@/components/ui/skeleton";
 import { useToast } from "@/components/ui/use-toast";
+import GameDetailsSection from "./GameDetailsSection";
+import LoadingGameDetailsSection from "./LoadingGameDetailsSection";
 
 const Game: FC = () => {
   const { t } = useTranslation();
@@ -30,24 +28,8 @@ const Game: FC = () => {
       navigate(URLS.GAMES);
     },
   });
-  const divRef = useRef<HTMLDivElement>(null);
+
   const { theme } = useTheme();
-
-  useEffect(() => {
-    const setWidthToHeight = () => {
-      if (divRef.current) {
-        const height = divRef.current.clientHeight;
-        divRef.current.style.width = `${height}px`;
-        divRef.current.style.minWidth = `${height}px`;
-      }
-    };
-
-    setWidthToHeight();
-    window.addEventListener("resize", setWidthToHeight);
-    return () => {
-      window.removeEventListener("resize", setWidthToHeight);
-    };
-  }, []);
 
   const color =
     theme === "system"
@@ -72,45 +54,7 @@ const Game: FC = () => {
           }}
         />
         <div className="absolute bottom-10 left-10 right-10 top-10 flex flex-row gap-8">
-          {isLoading ? (
-            <>
-              <div ref={divRef} className="h-full w-[265px]">
-                <Skeleton className="h-full w-full rounded-lg" />
-              </div>
-            </>
-          ) : (
-            game && (
-              <>
-                <div className="h-full overflow-hidden rounded-lg bg-section" ref={divRef}>
-                  <img
-                    src={game.image}
-                    alt={game.name}
-                    className="h-full w-full object-cover object-top"
-                  />
-                </div>
-                <ScrollArea>
-                  <div className="flex flex-col gap-2 lg:gap-4">
-                    <h1 className="text-xl font-bold xl:text-3xl">{game.name}</h1>
-                    <div className="flex flex-row flex-wrap gap-1">
-                      {game.categories.map(({ id, name }) => (
-                        <Badge key={id}>
-                          <Link to={`${URLS.CATEGORY_GAMES}/${id}`} className="text-sm">
-                            {name}
-                          </Link>
-                        </Badge>
-                      ))}
-                    </div>
-                    <p className="p-2 italic xl:text-lg 2xl:w-3/4">{game.shortDescription}</p>
-                    <div className="mt-auto flex flex-row flex-wrap gap-1">
-                      <TimeBadge time={game.playingTime} />
-                      <PlayersBadge minPlayers={game.minPlayers} maxPlayers={game.maxPlayers} />
-                      <AgeBadge age={game.age} />
-                    </div>
-                  </div>
-                </ScrollArea>
-              </>
-            )
-          )}
+          {isLoading ? <LoadingGameDetailsSection /> : game && <GameDetailsSection game={game} />}
         </div>
       </div>
       <div className="flex-grow rounded-lg bg-section"></div>
---
+++ frontend/src/pages/Game/GameDetailsSection.tsx
@@ -0,0 +1,61 @@
+import { FC, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { URLS } from "@/constants/urls";
+import { Game } from "@/types/Game";
+import { TimeBadge, PlayersBadge, AgeBadge } from "@/components/Badge";
+import { Badge } from "@/components/ui/badge";
+
+interface GameDetailsSectionProps {
+  game: Game;
+}
+
+const GameDetailsSection: FC<GameDetailsSectionProps> = ({ game }) => {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const setWidthToHeight = () => {
+      if (divRef.current) {
+        const height = divRef.current.clientHeight;
+        divRef.current.style.width = `${height}px`;
+        divRef.current.style.minWidth = `${height}px`;
+      }
+    };
+
+    setWidthToHeight();
+    window.addEventListener("resize", setWidthToHeight);
+    return () => {
+      window.removeEventListener("resize", setWidthToHeight);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="h-full overflow-hidden rounded-lg bg-section" ref={divRef}>
+        <img src={game.image} alt={game.name} className="h-full w-full object-cover object-top" />
+      </div>
+      <ScrollArea>
+        <div className="flex flex-col gap-2 lg:gap-4">
+          <h1 className="text-xl font-bold xl:text-3xl">{game.name}</h1>
+          <div className="flex flex-row flex-wrap gap-1">
+            {game.categories.map(({ id, name }) => (
+              <Badge key={id}>
+                <Link to={`${URLS.CATEGORY_GAMES}/${id}`} className="text-sm">
+                  {name}
+                </Link>
+              </Badge>
+            ))}
+          </div>
+          <p className="p-2 italic xl:text-lg 2xl:w-3/4">{game.shortDescription}</p>
+          <div className="flex flex-row flex-wrap gap-1">
+            <TimeBadge time={game.playingTime} />
+            <PlayersBadge minPlayers={game.minPlayers} maxPlayers={game.maxPlayers} />
+            <AgeBadge age={game.age} />
+          </div>
+        </div>
+      </ScrollArea>
+    </>
+  );
+};
+
+export default GameDetailsSection;
