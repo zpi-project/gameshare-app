a96db28799832f3f8b715cca837914ea60a358d5
Paulina Drzazga <paulina@halpress.team>
2023-11-08 18:24:14 +0100

    Added getMyGameInstances endpoint

--- backend/src/main/java/com/zpi/backend/gameInstance/GameInstanceService.java
+++ backend/src/main/java/com/zpi/backend/gameInstance/GameInstanceService.java
@@ -114,6 +114,11 @@ public class GameInstanceService {
                 new Pagination(gameInstancesPage.getTotalElements(), gameInstancesPage.getTotalPages()));
     }
 
+    public ResultsDTO<GameInstanceListDTO> getMyGameInstances(Optional<String> searchName, int size, int page, Authentication authentication) throws UserDoesNotExistException {
+        User user = userService.getUser(authentication);
+        return getUserGameInstances(user.getUuid(),searchName, size, page);
+    }
+
     public ResultsDTO<GameInstanceListDTO> getGameInstances(int size, int page, Optional<String> searchName, Optional<Long> categoryId, Optional<Integer> age,
                                                Optional<Integer> playersNumber, double latitude,
                                                double longitude) throws CategoryDoesNotExistException {
