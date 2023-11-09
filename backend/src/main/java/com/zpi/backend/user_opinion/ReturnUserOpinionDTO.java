163e1504286d419062ef8d14ff90ccd2114d76e2
JakubSamulski <kubasamulski@gmail.com>
2023-11-09 17:18:20 +0100

    paula uspokój się z tym review XD

--- backend/src/main/java/com/zpi/backend/user_opinion/ReturnUserOpinionDTO.java
+++ backend/src/main/java/com/zpi/backend/user_opinion/ReturnUserOpinionDTO.java
@@ -1,21 +1,22 @@
 package com.zpi.backend.user_opinion;
-import com.zpi.backend.user.User;
 import lombok.Data;
 import java.util.Date;
 
 @Data
 public class ReturnUserOpinionDTO {
+    private Long opinionId;
     private Date date;
     private String stars;
     private String description;
-    private User ratingUser;
+    private UserOpinionDTO ratingUser;
 
     //TODO: add isRatingUserOwner when we have game instances and reservations
 
     public ReturnUserOpinionDTO(UserOpinion userOpinion) {
+        this.opinionId = userOpinion.getId();
         this.date = userOpinion.getTimestamp();
         this.stars = String.valueOf(userOpinion.getStars());
         this.description = userOpinion.getDescription();
-        this.ratingUser = userOpinion.getRatingUser();
+        this.ratingUser = new UserOpinionDTO(userOpinion.getRatingUser());
     }
 }
