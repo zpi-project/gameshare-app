d482ec5f675649ada7bfcdc8ed93034fc374739e
Paulina Drzazga <paulina@halpress.team>
2023-11-10 10:09:47 +0100

    Fixed query

--- backend/src/main/java/com/zpi/backend/game/GameRepository.java
+++ backend/src/main/java/com/zpi/backend/game/GameRepository.java
@@ -58,8 +58,8 @@ public interface GameRepository extends JpaRepository<Game, Long> {
     @Query(value = "select u.uuid as uuid, u.first_name as firstName, u.last_name as lastName, " +
             "u.location_latitude as locationLatitude, u.location_longitude as locationLongitude, " +
             "u.avatar_link as avatarLink, " +
-            "COALESCE(u.avgRating, 0) as userRate, gi.uuid as gameInstanceUUID, " +
-            "g.name as gameName, COALESCE(gi.avgRating, 0) as gameInstanceRate," +
+            "u.avg_rating as userRate, gi.uuid as gameInstanceUUID, " +
+            "g.name as gameName, gi.avg_rating as gameInstanceRate," +
             "sqrt(pow(u.location_latitude - :userLatitude, 2) + pow(u.location_longitude - :userLongitude, 2)) as direction " +
             "from games g join game_instances gi on g.id = gi.game_id " +
             "join users u on gi.owner_id = u.id " +
