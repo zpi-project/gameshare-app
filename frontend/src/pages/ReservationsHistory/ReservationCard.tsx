8159098ebbb59a0412fbcd2f279b6349def93cc9
Maria Markowiak <maria.markowiak@gmail.com>
2023-11-17 23:10:57 +0100

    make ReservationCard responsive

--- frontend/src/pages/ReservationsHistory/ReservationCard.tsx
+++ frontend/src/pages/ReservationsHistory/ReservationCard.tsx
@@ -39,7 +39,7 @@ const ReservationCard: FC<ReservationCardProps> = ({
       className="flex flex-row gap-4 rounded-lg bg-card p-4 duration-200 hover:bg-accent"
       to={`${URLS.MY_RESERVATIONS}/${reservationId}`}
     >
-      <div className="h-40 w-40 overflow-hidden rounded-lg">
+      <div className="h-40 w-20 overflow-hidden rounded-lg xl:w-40">
         <img
           src={images[0]?.link ?? image}
           alt={`${name} image`}
@@ -47,7 +47,7 @@ const ReservationCard: FC<ReservationCardProps> = ({
         />
       </div>
       <section className="flex flex-grow flex-col gap-4">
-        <div className="flex flex-row flex-wrap gap-2">
+        <div className="flex flex-row flex-wrap justify-end gap-2">
           <h3 className="mr-auto text-2xl">{name}</h3>
           <Badge variant="secondary" className="lowercase">
             {t(`reservationStatuses.${asOwner ? "owner" : "renter"}.${status}`)}
@@ -56,30 +56,34 @@ const ReservationCard: FC<ReservationCardProps> = ({
         </div>
         <Separator />
         <div className="flex flex-row justify-between gap-2">
-          <div className="flex flex-row gap-3">
-            <div className="h-20 w-20 overflow-hidden rounded-full">
+          <div className="hidden flex-row gap-3 lg:flex">
+            <div className="h-16 w-16 overflow-hidden rounded-full xl:h-20 xl:w-20">
               <img
                 src={asOwner ? renterAvatar : ownerAvatar}
                 alt={getFullname(asOwner ? renter : owner)}
                 className="h-full w-full object-cover"
               />
             </div>
-            <div className="flex h-full flex-col gap-1 ">
+            <div className="hidden h-full flex-col gap-1 xl:flex ">
               <h4 className="text-xl">{getFullname(asOwner ? renter : owner)}</h4>
               {<Stars count={Math.round(asOwner ? renterRating : ownerRating)} size={24} />}
             </div>
           </div>
-          <div className="flex min-w-[300px] flex-row justify-between gap-4">
-            <div>
+          <div className="w-full lg:w-max lg:min-w-[300px]">
+            <div className="flex flex-row justify-between gap-4">
               <p>{t("reservationNumber")}</p>
-              <p>{t("startDate")}</p>
-              <p>{t("endDate")}</p>
-              <p>{t("days")}</p>
-            </div>
-            <div className="text-right">
               <p>{reservationId}</p>
+            </div>
+            <div className="flex flex-row justify-between gap-4">
+              <p>{t("startDate")}</p>
               <p>{t("dateFormat", { date: new Date(startDate) })}</p>
+            </div>
+            <div className="flex flex-row justify-between gap-4">
+              <p>{t("endDate")}</p>
               <p>{t("dateFormat", { date: new Date(endDate) })}</p>
+            </div>
+            <div className="flex flex-row justify-between gap-4">
+              <p>{t("days")}</p>
               <p>{duration}</p>
             </div>
           </div>
