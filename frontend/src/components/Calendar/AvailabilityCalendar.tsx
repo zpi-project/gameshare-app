1e469068a3fa7e71b7c5c0a0f4d2fb384a462234
Maria Markowiak <maria.markowiak@gmail.com>
2023-11-19 11:47:54 +0100

    Add game calendar component

---
+++ frontend/src/components/Calendar/AvailabilityCalendar.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+const AvailabilityCalendar: FC = () => {
+  return (
+    <div>AvailabilityCalendar</div>
+  )
+}
+
+export default AvailabilityCalendar;
---
+++ frontend/src/pages/GameInstance/GameCalendar.tsx
@@ -0,0 +1,10 @@
+import { DialogContent } from '@/components/ui/dialog';
+import { FC } from 'react'
+
+const GameCalendar: FC = () => {
+  return (
+    <DialogContent>GameCalendar</DialogContent>
+  )
+}
+
+export default GameCalendar;
