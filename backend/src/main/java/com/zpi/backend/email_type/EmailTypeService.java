d59bfe76212f1c7ea36be70d51f2486b5d4832d0
Paulina Drzazga <paulina@halpress.team>
2023-11-20 19:30:40 +0100

    Renamed property status to type in EmailType class

--- backend/src/main/java/com/zpi/backend/email_type/EmailTypeService.java
+++ backend/src/main/java/com/zpi/backend/email_type/EmailTypeService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 public class EmailTypeService {
     private final EmailTypeRepository emailTypeRepository;
     public EmailType findEmailTypeByStatus(String status) throws EmailTypeDoesNotExists {
-       return emailTypeRepository.findEmailTypeByStatus(status)
+       return emailTypeRepository.findEmailTypeByType(status)
                .orElseThrow(() -> new EmailTypeDoesNotExists(status + " status does not exists."));
     }
 }
