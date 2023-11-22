508a506a762ed9dd146a64e92bd552cb5c373174
Maria Markowiak <maria.markowiak@gmail.com>
2023-11-22 16:17:59 +0100

    add reservation details section

--- frontend/src/pages/ReservationDetails/ReservationDetailsOwner.tsx
+++ frontend/src/pages/ReservationDetails/ReservationDetailsOwner.tsx
@@ -1,12 +1,36 @@
 import { FC } from "react";
+import { useTranslation } from "react-i18next";
 import { ReservationDetails } from "@/types/Reservation";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import ReservationDetailsSection from "./ReservationDetailsSection";
 
 interface ReservationDetailsOwnerProps {
   reservation: ReservationDetails;
 }
 
-const ReservationDetailsOwner: FC<ReservationDetailsOwnerProps> = ({reservation}) => {
-  return <div>owner ReservationDetails</div>;
+const ReservationDetailsOwner: FC<ReservationDetailsOwnerProps> = ({
+  reservation: { reservation },
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <ScrollArea className="h-full w-full xl:h-max">
+      <div className="flex h-full w-full flex-col gap-4 xl:h-[calc(100vh-48px)] xl:flex-row">
+        <div className="flex min-h-[100%] flex-grow flex-col gap-4">
+          <ReservationDetailsSection reservation={reservation} />
+          <div className="flex-grow rounded-lg bg-section p-4">renter opinion about you</div>
+          <div className="flex-grow rounded-lg bg-section p-4">renter opinion your game</div>
+        </div>
+        <div className="flex flex-grow flex-col gap-4">
+          <div className="relative flex-grow rounded-lg bg-section p-4">renter details</div>
+          <div className="flex-grow rounded-lg bg-section p-4">your opinion about renter</div>
+        </div>
+        <div className="relative flex flex-grow flex-col gap-4 rounded-lg bg-section p-4 xl:h-full">
+          game + game calendar
+        </div>
+      </div>
+    </ScrollArea>
+  );
 };
 
 export default ReservationDetailsOwner;
---
+++ frontend/src/pages/ReservationDetails/ReservationDetailsSection/ReservationDetailsStatus.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface ReservationDetailsStatusProps {
+  reservationId: string;
+}
+
+const ReservationDetailsStatus: FC<ReservationDetailsStatusProps> = ({ reservationId }) => {
+  return <div>ReservationDetailsStatus</div>;
+};
+
+export default ReservationDetailsStatus;
