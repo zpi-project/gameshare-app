e0f44612a1ee77c609f2d663f5f606f202877d4a
Maria Markowiak <maria.markowiak@gmail.com>
2023-11-23 00:39:52 +0100

    fix GameInstanceDetails

--- frontend/src/pages/ReservationDetails/GameInstanceDetails.tsx
+++ frontend/src/pages/ReservationDetails/GameInstanceDetails.tsx
@@ -1,37 +1,50 @@
+import { FC } from "react";
+import { useTranslation } from "react-i18next";
+import { GameInstanceDetails as GameInstanceDetailsType } from "@/types/GameInstance";
 import { PriceBadge } from "@/components/Badge";
 import { Stars } from "@/components/Stars";
 import { Badge } from "@/components/ui/badge";
-import { GameInstanceDetails } from "@/types/GameInstance";
-import { FC } from "react";
-import { useTranslation } from "react-i18next";
 
 interface GameInstanceDetailsProps {
-  gameInstance: GameInstanceDetails;
+  gameInstance: GameInstanceDetailsType;
 }
 
-const GameInstanceDetails: FC<GameInstanceDetailsProps> = ({ gameInstance: { game: { name, image }, images, pricePerDay, avgRating, description } }) => {
+const GameInstanceDetails: FC<GameInstanceDetailsProps> = ({
+  gameInstance: {
+    game: { image, name },
+    description,
+    avgRating,
+    pricePerDay,
+    images,
+  },
+}) => {
   const { t } = useTranslation();
 
   return (
     <div className="flex flex-col gap-4">
-      <h3 className="uppercase text-xl">{t("gameDetails")}</h3>
+      <h3 className="text-xl uppercase">{t("gameDetails")}</h3>
       <div className="flex flex-col gap-4">
-        <p className="flex flex-row justify-between flex-wrap">
+        <p className="flex flex-row flex-wrap justify-between">
           <h4 className="text-lg font-semibold">{name}</h4>
           <PriceBadge price={pricePerDay} />
         </p>
         <div className="flex flex-row gap-4">
-          <div className="h-[190px] min-w-[190px] w-[190px] overflow-hidden rounded-lg bg-section flex-grow">
-            <img src={images.length ? images[0].link : image} alt={name} className="h-full w-full object-cover object-top" />
+          <div className="h-[190px] w-[190px] min-w-[190px] flex-grow overflow-hidden rounded-lg bg-section">
+            <img
+              src={images.length ? images[0].link : image}
+              alt={name}
+              className="h-full w-full object-cover object-top"
+            />
           </div>
-          <div className="flex flex-col gap-2 flex-grow">
+          <div className="flex flex-grow flex-col gap-2">
             <div className="ml-auto">
-              {avgRating > 0 ? <Stars count={Math.round(avgRating)} variant="secondary" /> : <Badge variant="secondary">{t("gameNoOpinions")}</Badge>}
+              {avgRating > 0 ? (
+                <Stars count={Math.round(avgRating)} variant="secondary" />
+              ) : (
+                <Badge variant="secondary">{t("gameNoOpinions")}</Badge>
+              )}
             </div>
-            <p className="break-words italic line-clamp-6">
-              {description}
-             
-            </p>
+            <p className="line-clamp-6 break-words italic">{description}</p>
           </div>
         </div>
       </div>
