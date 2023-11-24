d3504ae220830a050238031a0b7dd98e47430668
Aleksandra <aleserwicka@gmail.com>
2023-11-24 23:21:32 +0100

    gce pipeline

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -12,7 +12,7 @@
 
 # sudo apt-get -y update
 # sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo apt-get -y install git
-git clone https://github.com/zpi-project/gameshare-app.git
-cd gameshare-app
+# sudo apt-get -y install git
+# git clone https://github.com/zpi-project/gameshare-app.git
+# cd gameshare-app
 # sudo docker compose-up
