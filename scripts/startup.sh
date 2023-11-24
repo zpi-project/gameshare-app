9dc67fcbe886381ba57dc29c7726b600fc8765db
Aleksandra <aleserwicka@gmail.com>
2023-11-24 23:48:59 +0100

    gce pipeline

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,18 @@
 
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install git
 git clone https://github.com/zpi-project/gameshare-app.git
 cd gameshare-app
 sudo docker compose-up
