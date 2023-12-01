642a66328326625c0e0fc6f931c2be807c75afbf
Paulina Drzazga <paulina@halpress.team>
2023-12-01 19:12:05 +0100

    Changed saving statuses, types and roles

--- backend/src/main/java/com/zpi/backend/role/RoleRepository.java
+++ backend/src/main/java/com/zpi/backend/role/RoleRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role getRoleByName(String name);
+    boolean existsRoleByName(String name);
 
 }
 
