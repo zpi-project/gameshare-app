b04827888c0695087b3c845d0b867a65648c8b82
Aleksandra <aleserwicka@gmail.com>
2023-12-04 11:06:55 +0100

    .

--- frontend/src/pages/ReservationDetails/ReservationDetailsOwner/OpinionAboutRenter.tsx
+++ frontend/src/pages/ReservationDetails/ReservationDetailsOwner/OpinionAboutRenter.tsx
@@ -1,17 +1,18 @@
 import { FC, useState } from "react";
-import { Form, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import { queryClient } from "@cypress/support/queryClient";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation } from "@tanstack/react-query";
-import { t } from "i18next";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { z } from "zod";
+import { Game } from "@/types/Game";
 import { NewOpinion, Opinion } from "@/types/Opinion";
-import { User } from "@/types/User";
 import { UserApi } from "@/api/UserApi";
 import { Stars, StarsInput } from "@/components/Stars";
 import { Button } from "@/components/ui/button";
-import { FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
+import { FormField, FormItem, FormLabel, FormControl, Form } from "@/components/ui/form";
 import { Textarea } from "@/components/ui/textarea";
+import { toast } from "@/components/ui/use-toast";
 
 interface OpinionAboutRenterProps {
   renterOpinion: Opinion | null;
@@ -27,12 +28,23 @@ const OpinionAboutRenter: FC<OpinionAboutRenterProps> = ({
   reservationId,
 }) => {
   const { t } = useTranslation();
-  const { mutateAsync: addOpinion } = useMutation({
+  const queryClient = useQueryClient();
+  const { mutate: addOpinion } = useMutation({
     mutationFn: (opinion: NewOpinion) => UserApi.addOpinion(opinion),
+    onError: () => {
+      toast({
+        title: t("registerErrorTitle"),
+        description: t("tryRefreshing"),
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      toast({
+        description: t("registerSuccessDescription"),
+      });
+      queryClient.invalidateQueries(["reservation", { reservationId }]);
+    },
   });
-
-  const [stars, setStars] = useState<number>(0);
-  // const [description, setDescription] = useState<string>("");
 
   const formSchema = z.object({
     stars: z.number({
@@ -46,11 +58,27 @@ const OpinionAboutRenter: FC<OpinionAboutRenterProps> = ({
         message: t("gameInstaneDescriptionMin"),
       })
       .max(500, { message: t("gameInstanceDescriptionMax") }),
+    ratedUserUUID: z.string(),
+    reservationId: z.string(),
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      reservationId,
+      ratedUserUUID: renterUUID,
+    },
   });
+
+  // const handleFormSubmit = async (data: NewOpinion) => {
+
+  //     const newOpinion = addOpinion(data);
+
+  //     toast({
+  //       title: t("gameInstanceAdded"),
+  //     });
+
+  // };
 
   return (
     <div className="flex h-full  flex-grow flex-col gap-4 rounded-lg bg-section p-4">
@@ -66,51 +94,40 @@ const OpinionAboutRenter: FC<OpinionAboutRenterProps> = ({
         </>
       ) : canAddRenterOpinion ? (
         <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(() =>
-              addOpinion({
-                ratedUserUUID: renterUUID,
-                stars: stars,
-                description: form.getValues("description"),
-                reservationId: reservationId,
-              }),
-            )}
-          >
+          <form onSubmit={form.handleSubmit(data => addOpinion(data))}>
             <div className="flex h-full w-full flex-col gap-1.5">
               <div className="flex flex-row justify-between p-1">
-                <h3 className="text-xl uppercase">{t("reservationDetails.renter.gameOpinion")}</h3>
+                <h3 className="text-xl uppercase">{t("reservationDetails.owner.renterOpinion")}</h3>
                 <FormField
                   control={form.control}
                   name="stars"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>{t("addGameDesc")}</FormLabel>
                       <FormControl>
-                        <div className="grid h-[370px] w-full gap-2.5 rounded-lg bg-card p-4">
-                          <StarsInput onChange={setStars} />
-                        </div>
+                        <StarsInput
+                          onChange={(stars: number) => {
+                            field.onChange(stars);
+                          }}
+                        />
                       </FormControl>
                     </FormItem>
                   )}
                 />
               </div>
-              <div className="flex h-full w-full flex-row justify-between gap-1">
+              <div className="flex h-full w-full gap-1">
                 <FormField
                   control={form.control}
                   name="description"
                   render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{t("addGameDesc")}</FormLabel>
+                    <FormItem className="h-[150px] w-full">
                       <FormControl>
-                        <div className="grid h-[370px] w-full gap-2.5 rounded-lg bg-card p-4">
-                          <Textarea
-                            placeholder={t("typeHere")}
-                            id="message-2"
-                            spellCheck={false}
-                            className="h-full resize-none"
-                            {...field}
-                          />
-                        </div>
+                        <Textarea
+                          placeholder={t("typeHere")}
+                          id="message-2"
+                          spellCheck={false}
+                          className="h-full resize-none"
+                          {...field}
+                        />
                       </FormControl>
                     </FormItem>
                   )}
