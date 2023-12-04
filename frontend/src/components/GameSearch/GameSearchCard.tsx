a304198a7bfce2c08a45afbf007ae3f8cc43dfdc
Maria Markowiak <maria.markowiak@gmail.com>
2023-12-04 13:42:07 +0100

    init calendar and game search tests

--- frontend/src/components/GameSearch/GameSearchCard.tsx
+++ frontend/src/components/GameSearch/GameSearchCard.tsx
@@ -13,13 +13,21 @@ const GameSearchCard: FC<GameSearchCardProps> = ({ game, onClick }) => {
   return (
     <div
       className="relative flex h-[100px] w-full cursor-pointer flex-row gap-2 rounded-lg bg-card p-2 shadow duration-200 hover:bg-accent lg:h-max"
+      data-test="game-search-card"
       onClick={() => onClick && onClick(game)}
     >
       <div className="h-16 w-16 overflow-hidden rounded-lg">
-        <img src={image} alt={name} className="h-full w-full object-cover object-top" />
+        <img
+          src={image}
+          alt={name}
+          className="h-full w-full object-cover object-top"
+          data-test="image"
+        />
       </div>
-      <h4 className="text-xl text-primary">{name}</h4>
-      <div className="absolute bottom-2 right-2 flex flex-row gap-1">
+      <h4 className="text-xl text-primary" data-test="name">
+        {name}
+      </h4>
+      <div className="absolute bottom-2 right-2 flex flex-row gap-1" data-test="badges">
         <TimeBadge time={playingTime} />
         <PlayersBadge minPlayers={minPlayers} maxPlayers={maxPlayers} />
         <AgeBadge age={age} />
