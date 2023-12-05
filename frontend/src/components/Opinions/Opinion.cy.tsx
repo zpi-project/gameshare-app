d3dc025b8742889e4e42af72a0a2aa1075594724
Maria Markowiak <maria.markowiak@gmail.com>
2023-12-05 13:26:19 +0100

    define tests for opinions

---
+++ frontend/src/components/Opinions/Opinion.cy.tsx
@@ -0,0 +1,6 @@
+describe("<Opinion />", () => {
+  it("should render correctly all informations", () => {}),
+    it("shows 'see more' option if description is too long", () => {}),
+    it("toggles description on see more and see less button", () => {}),
+    it("doesn't show 'see more' button if description is no long enough", () => {});
+});
---
+++ frontend/src/components/Opinions/Opinions.cy.tsx
@@ -0,0 +1,7 @@
+describe("<Opinions />", () => {
+  it("displays correcly all opinions for a user", () => {});
+  it("correcly shows loading state", () => {});
+  it("displays error message when fetching opinions fails", () => {});
+  it("displays 'no opinions' message when user has no opinions", () => {});
+  it("displays no opinions if user is not provided", () => {});
+});
