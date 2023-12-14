87188df8997d8ad68b27dbb790d6c354a22acfc7
JakubSamulski <kubasamulski1@gmail.com>
2023-12-14 18:07:21 +0100

    .

--- frontend/src/components/Stars/StarsInput.cy.tsx
+++ frontend/src/components/Stars/StarsInput.cy.tsx
@@ -2,13 +2,13 @@ import StarsInput from "./StarsInput";
 
 describe("<StarsInput />", () => {
   it("renders correctly", () => {
-    cy.mount(<StarsInput />);
+    cy.mount(<StarsInput onChange={cy.stub()} />);
     cy.getBySel("star").should("have.length", 5);
     cy.getBySel("star").eq(3).click();
   });
 
   it("renders correctly in secondary variant", () => {
-    cy.mount(<StarsInput variant="secondary" />);
+    cy.mount(<StarsInput variant="secondary" onChange={cy.stub()} />);
     cy.getBySel("star").should("have.length", 5);
     cy.getBySel("star").eq(4).click();
     cy.getBySel("star").eq(2).click();
